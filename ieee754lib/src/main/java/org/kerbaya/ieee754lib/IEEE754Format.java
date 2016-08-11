1f48e35cf04e335307786e910b3a11296ce72cb0
oldhen <oldhen@users.noreply.github.com>
2016-08-11 08:20:26 -0300

    Signed-off-by: oldhen <oldhen@users.noreply.github.com>

--- ieee754lib/src/main/java/org/kerbaya/ieee754lib/IEEE754Format.java
+++ ieee754lib/src/main/java/org/kerbaya/ieee754lib/IEEE754Format.java
@@ -27,10 +27,55 @@ import java.math.BigInteger;
 
 public class IEEE754Format
 {
+	/**
+	 * <dl>
+	 * <dt>IEEE 754 name</dt><dd>binary16</dd>
+	 * <dt>Sign bit</dt><dd>1 bit</dd>
+	 * <dt>Exponent width</dt><dd>5 bits</dd>
+	 * <dt>Significand precision</dt><dd>11 bits (10 explicitly stored)</dd>
+	 * </dl>
+	 */
 	public static final IEEE754Format HALF = new IEEE754Format(5, 10, 15);
+	
+	/**
+	 * <dl>
+	 * <dt>IEEE 754 name</dt><dd>binary32</dd>
+	 * <dt>Sign bit</dt><dd>1 bit</dd>
+	 * <dt>Exponent width</dt><dd>8 bits</dd>
+	 * <dt>Significand precision</dt><dd>24 bits (23 explicitly stored)</dd>
+	 * </dl>
+	 */
 	public static final IEEE754Format SINGLE = new IEEE754Format(8, 23, 127);
+	
+	/**
+	 * <dl>
+	 * <dt>IEEE 754 name</dt><dd>binary64</dd>
+	 * <dt>Sign bit</dt><dd>1 bit</dd>
+	 * <dt>Exponent width</dt><dd>11 bits</dd>
+	 * <dt>Significand precision</dt><dd>53 bits (52 explicitly stored)</dd>
+	 * </dl>
+	 */
 	public static final IEEE754Format DOUBLE = new IEEE754Format(11, 52, 1023);
+	
+	/**
+	 * <dl>
+	 * <dt>IEEE 754 name</dt><dd>binary128</dd>
+	 * <dt>Sign bit</dt><dd>1 bit</dd>
+	 * <dt>Exponent width</dt><dd>15 bits</dd>
+	 * <dt>Significand precision</dt><dd>113 bits (112 explicitly stored)</dd>
+	 * </dl>
+	 */
 	public static final IEEE754Format QUADRUPLE = new IEEE754Format(15, 112, 16383);
+	
+	/**
+	 * <dl>
+	 * <dt>IEEE 754 name</dt><dd>binary256</dd>
+	 * <dt>Sign bit</dt><dd>1 bit</dd>
+	 * <dt>Exponent width</dt><dd>19 bits</dd>
+	 * <dt>Significand precision</dt><dd>237 bits (236 explicitly stored)</dd>
+	 * </dl>
+	 */
+	public static final IEEE754Format OCTUPLE = new IEEE754Format(19, 236, 262143);
 
 	private final int exponentLength;
 	private final int mantissaLength;
